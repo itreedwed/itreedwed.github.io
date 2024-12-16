b8d31023b77636c2351bd7c21e1e9e9ef07ae170
itreedwed <itreedwed@gmail.com>
2024-12-17 02:47:32 +0700

    Update gulpfile.mjs

--- gulpfile.mjs
+++ gulpfile.mjs
@@ -25,13 +25,13 @@ const resources = {
   less: "src/styles/**/*.less",
   svgSprite: "src/assets/svg-sprite/*.svg",
   static: [
-    "src/assets/icons/**/*.*",
-    "src/assets/favicons/**/*.*",
+   // "src/assets/icons/**/*.*",
+   // "src/assets/favicons/**/*.*",
     "src/assets/fonts/**/*.{woff,woff2}",
-    "src/assets/video/**/*.{mp4,webm}",
-    "src/assets/audio/**/*.{mp3,ogg,wav,aac}",
-    "src/json/**/*.json",
-    "src/php/**/*.php"
+   // "src/assets/video/**/*.{mp4,webm}",
+   // "src/assets/audio/**/*.{mp3,ogg,wav,aac}",
+   // "src/json/**/*.json",
+   // "src/php/**/*.php"
   ]
 };
 // Gulp Tasks:
